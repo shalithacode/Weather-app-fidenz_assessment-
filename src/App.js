8b70989d589af872262147317569e46ee567935d
shalithacode <shalithalakruwan1998@gmail.com>
2023-02-26 17:45:50 +0530

    split components

--- src/App.js
+++ src/App.js
@@ -1,70 +1,13 @@
-import React, { useState, useEffect } from "react";
 import "./App.css";
-import { Grid } from "@mui/material";
-import Card from "./Components/Card";
+
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import CityList from "./Components/CityList";
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("cities.json")
-      .then((response) => response.json())
-      .then((jsonData) => {
-        const API_KEY = "895284fb2d2c50a520ea537456963d9c";
-        const CITY_CODE = jsonData.List.map((city) => city.CityCode).join(",");
-        const weatherAPI = `https://api.openweathermap.org/data/2.5/group?id=${CITY_CODE}&units=metric&appid=${API_KEY}`;
-
-        fetch(weatherAPI)
-          .then((response) => response.json())
-          .then((data) => {
-            setData(
-              data.list.map((i) => {
-                return {
-                  description: i.weather[0].description,
-                  temp: i.main.temp,
-                  dt: i.dt,
-                  id: i.id,
-                  name: i.name,
-                };
-              })
-            );
-          })
-          .catch((error) => console.error(error));
-      })
-      .catch((error) => console.error(error));
-  }, []);
-
-  const cache = new Map();
-
-  function getWeatherData() {
-    const now = new Date().getTime();
-
-    if (cache.has("WeatherData")) {
-      const cachedData = cache.get("WeatherData");
-      if (now - cachedData.time < 1000 * 60 * 5) {
-        return cachedData.data;
-      } else {
-        cache.delete("WeatherData");
-      }
-    }
-
-    cache.set("WeatherData", { data: data, time: now });
-    return data;
-  }
-
   return (
     <>
-      <Header></Header>
-      <Grid container rowSpacing={1}>
-        {getWeatherData().map((citiData, i) => {
-          return (
-            <Grid key={i} item xs={12} md={6}>
-              <Card citiData={citiData} />
-            </Grid>
-          );
-        })}
-      </Grid>
+      <Header />
+      <CityList />
       <Footer />
     </>
   );
---
+++ src/Components/CityList.js
@@ -0,0 +1,69 @@
+import { Grid } from "@mui/material";
+import Card from "./Card";
+import React, { useState, useEffect } from "react";
+
+function CityList() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("cities.json")
+      .then((response) => response.json())
+      .then((jsonData) => {
+        const API_KEY = "895284fb2d2c50a520ea537456963d9c";
+        const CITY_CODE = jsonData.List.map((city) => city.CityCode).join(",");
+        const weatherAPI = `https://api.openweathermap.org/data/2.5/group?id=${CITY_CODE}&units=metric&appid=${API_KEY}`;
+
+        fetch(weatherAPI)
+          .then((response) => response.json())
+          .then((data) => {
+            setData(
+              data.list.map((i) => {
+                return {
+                  description: i.weather[0].description,
+                  temp: i.main.temp,
+                  dt: i.dt,
+                  id: i.id,
+                  name: i.name,
+                };
+              })
+            );
+          })
+          .catch((error) => console.error(error));
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const cache = new Map();
+
+  function getWeatherData() {
+    const now = new Date().getTime();
+
+    if (cache.has("WeatherData")) {
+      const cachedData = cache.get("WeatherData");
+      if (now - cachedData.time < 1000 * 60 * 5) {
+        return cachedData.data;
+      } else {
+        cache.delete("WeatherData");
+      }
+    }
+
+    cache.set("WeatherData", { data: data, time: now });
+    return data;
+  }
+
+  return (
+    <>
+      <Grid container rowSpacing={1}>
+        {getWeatherData().map((citiData, i) => {
+          return (
+            <Grid key={i} item xs={12} md={6}>
+              <Card citiData={citiData} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
+  );
+}
+
+export default CityList;
